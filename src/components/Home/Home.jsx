cebd0f5872c5145a0342c2f855bd25e4c9b3fe10
ChloeSAPage <chloesapagepersonal@gmail.com>
2024-05-20 17:04:40 +0100

    Add home main content and styling for mobile

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,21 +2,31 @@ import ButtonPurple from "../Reusable/ButtonPurple/ButtonPurple";
 import ButtonWhite from "../Reusable/ButtonWhite/ButtonWhite";
 import styles from "./Home.module.css";
 
-
 function Home() {
   return (
     <div className={styles.wrapper}>
-      <p>Home</p>
-      <h1>This is a heading</h1>
-      <h2>This is a heading</h2>
-      <h3>This is a heading</h3>
-      <h4>This is a heading</h4>
-      <p>This is a paragraph</p>
-      <ButtonPurple />
-      <ButtonWhite />
-      <br />
-      <a href="#">This is a link</a>
-      <div className={styles.shadow}>This is a div</div>
+      <div className={`${styles.shadow} ${styles.padding}`}>
+        <h2 className={styles.margin}>We do Recipes</h2>
+        <h2 className={`${styles.margin} ${styles.via}`}>(via ChatGPT)</h2>
+        <p className={styles.para}>
+          Do you want a list of dishes that actually have no recipe at all? Ask
+          us a for them, we'll secretly pass it on to ChatGPT and will return
+          the result pretending that we generated it ourselves. Even this
+          paragraph was stolen.
+        </p>
+        <img src="/images/recipe-book.jpg" alt="image" />
+        <div className={styles.center}>
+          <p className={styles.para}>
+            Post a question about recipes and see some suggestions from our
+            specially trained Al
+          </p>
+          <ButtonPurple text="Ask our AI!" />
+          <p className={styles.para}>
+            Then don't actually make anything yourself, buy from us!
+          </p>
+          <ButtonWhite text="Visit our Shop" />
+        </div>
+      </div>
     </div>
   );
 }
