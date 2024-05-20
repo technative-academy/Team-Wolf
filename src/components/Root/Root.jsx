1db23e4f44bb078d44ad6b19318acb1840538200
Kamila W <kamila.wilczynska92@gmail.com>
2024-05-20 12:53:24 +0100

    fix typo

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,14 +1,12 @@
 import styles from "./Root.module.css";
 
 import SiteHeader from "../SiteHeader/SiteHeader";
-import SiteNav from "../SiteNav/SiteNav";?
 import Footer from "../Footer/Footer";
 import MainContent from "../MainContent/MainContent";
 
 function Root() {
   return (
     <div className={styles.wrapper}>
-      
       <SiteHeader />
       <MainContent />
       <Footer />
