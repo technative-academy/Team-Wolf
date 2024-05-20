b4d688374ccc9432ce6e91ad8dc9934062f7bbae
ChloeSAPage <chloesapagepersonal@gmail.com>
2024-05-20 14:54:55 +0100

    Add heading styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,11 @@ function Home() {
   return (
     <div className={styles.wrapper}>
       <p>Home</p>
+      <h1>This is a heading</h1>
+      <h2>This is a heading</h2>
+      <h3>This is a heading</h3>
+      <h4>This is a heading</h4>
+      <p>This is a paragraph</p>
     </div>
   );
 }
