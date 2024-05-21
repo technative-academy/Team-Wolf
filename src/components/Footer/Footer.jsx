1e64e7739aad08a8dc62fbf1a29831768d7fab7e
Helder Balbino <helderbalbino@googlemail.com>
2024-05-21 10:53:56 +0100

    add the link tag instead of the achor tag

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,20 @@
 import styles from "./Footer.module.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return <div className={styles.wrapper}>
-    <p> <a href="#">Terms and Conditions</a></p>
-    <p><a href="#">Cookies</a></p>
+    <Link to='#'>
+      <p> Terms and Conditions </p>
+    </Link>
+
+
+
+
+
+    
+    <Link to='#'>
+      <p>Cookies</p>
+    </Link>
     <p className="copyright"><span>&#169;</span> 2024</p>
   </div>;
 }
