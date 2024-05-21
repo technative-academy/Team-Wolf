c423daa2481c08f69bb04e4ff2defe755e4e08fc
Kamila W <kamila.wilczynska92@gmail.com>
2024-05-21 16:50:46 +0100

    change the header accorging to the location

--- src/components/SiteHeader/SiteHeader.jsx
+++ src/components/SiteHeader/SiteHeader.jsx
@@ -1,11 +1,26 @@
 import styles from "./SiteHeader.module.css";
 import SiteNav from "../SiteNav/SiteNav";
+import { useLocation } from "react-router-dom";
 
 function SiteHeader() {
+  const location = useLocation();
+
+  const getHeaderText = () => {
+    switch (location.pathname) {
+      case "/":
+        return "Welcome to AI Cooking Assistant";
+      case "/ask":
+        return "Ask AI Cooking Assistant";
+      case "/about":
+        return "About AI Cooking Assistant";
+      default:
+        return "Hi";
+    }
+  };
   return (
     <div className={styles.wrapper}>
       <SiteNav />
-      <h1 className={styles.mainHeading}>Welcome to AI Cooking Assistant</h1>
+      <h1 className={styles.mainHeading}>{getHeaderText()}</h1>
       <h3 className={styles.subheading}>
         With us, you will never struggle to find meal ideas again.
       </h3>
