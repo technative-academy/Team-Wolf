896e22c081e772f0007abb22f7ce4424f12effd6
Kamila W <kamila.wilczynska92@gmail.com>
2024-05-21 14:36:50 +0100

    fix default content display

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,29 +1,38 @@
+import { Outlet } from "react-router-dom";
+import { useState } from "react";
 import styles from "./About.module.css";
 import { NavLink } from "react-router-dom";
-import { Outlet } from "react-router-dom";
+import AboutApp from "../AboutTheApp/AboutTheApp";
 
 function About() {
+  const [activeLink, setActiveLink] = useState("about-app");
+
   return (
     <div className={styles.wrapper}>
       <div className={` ${styles.card} ${styles.shadow} ${styles.padding}`}>
         <div className={styles.buttonWrapper}>
           <NavLink
             to="about-app"
-            className={({ isActive }) =>
-              isActive ? `${styles.link} ${styles.activeLink}` : styles.link
-            }
+            className={`${styles.link} ${
+              activeLink === "about-app" ? styles.activeLink : ""
+            }`}
+            onClick={() => setActiveLink("about-app")}
           >
             About the App
           </NavLink>
           <NavLink
             to="about-team"
-            className={({ isActive }) =>
-              isActive ? `${styles.link} ${styles.activeLink}` : styles.link
-            }
+            className={`${styles.link} ${
+              activeLink === "about-team" ? styles.activeLink : ""
+            }`}
+            onClick={() => setActiveLink("about-team")}
           >
             About the Team
           </NavLink>
         </div>
+
+        {activeLink === "about-app" && <AboutApp />}
+
         <Outlet />
       </div>
     </div>
