49206a5bf3fc626109621b5f5e3871758a11c3b4
Helder Balbino <helderbalbino@googlemail.com>
2024-05-21 10:23:29 +0100

    create a footer with terms and conditions and cookies links, and a copyright logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,11 @@
 import styles from "./Footer.module.css";
 
 function Footer() {
-  return <div className={styles.wrapper}>SiteFooter</div>;
+  return <div className={styles.wrapper}>
+    <p> <a href="#">Terms and Conditions</a></p>
+    <p><a href="#">Cookies</a></p>
+    <p className="copyright"><span>&#169;</span> 2024</p>
+  </div>;
 }
 
 export default Footer;
