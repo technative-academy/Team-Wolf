6b1cad13255122b839773c158f59b7f1d0215b07
Kamila W <kamila.wilczynska92@gmail.com>
2024-05-21 14:06:02 +0100

    add subpages to about page, add some styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,31 @@
 import styles from "./About.module.css";
+import { NavLink } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 function About() {
   return (
     <div className={styles.wrapper}>
-      <p>Ask</p>
+      <div className={` ${styles.card} ${styles.shadow} ${styles.padding}`}>
+        <div className={styles.buttonWrapper}>
+          <NavLink
+            to="about-app"
+            className={({ isActive }) =>
+              isActive ? `${styles.link} ${styles.activeLink}` : styles.link
+            }
+          >
+            About the App
+          </NavLink>
+          <NavLink
+            to="about-team"
+            className={({ isActive }) =>
+              isActive ? `${styles.link} ${styles.activeLink}` : styles.link
+            }
+          >
+            About the Team
+          </NavLink>
+        </div>
+        <Outlet />
+      </div>
     </div>
   );
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,37 @@
+// import Root from "./components/Root/Root";
+// import Home from "./components/Home/Home";
+// import About from "./components/About/About";
+// import Ask from "./components/Ask/Ask";
+
+// const routes = [
+//   {
+//     path: "/",
+//     element: <Root />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "ask",
+//         element: <Ask />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//     ],
+//   },
+// ];
+
+// export default routes;
+
 import Root from "./components/Root/Root";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Ask from "./components/Ask/Ask";
+import AboutApp from "./components/AboutTheApp/AboutTheApp";
+import AboutTeam from "./components/AboutTheTeam/AboutTheTeam";
 
 const routes = [
   {
@@ -19,6 +49,16 @@ const routes = [
       {
         path: "about",
         element: <About />,
+        children: [
+          {
+            path: "about-app",
+            element: <AboutApp />,
+          },
+          {
+            path: "about-team",
+            element: <AboutTeam />,
+          },
+        ],
       },
     ],
   },
