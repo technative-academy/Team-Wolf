20a4931981ac611efa3ca464be2de84efbd7ec9f
Kamila W <kamila.wilczynska92@gmail.com>
2024-05-21 12:57:47 +0100

    add extra styles, ass header

--- src/components/SiteHeader/SiteHeader.jsx
+++ src/components/SiteHeader/SiteHeader.jsx
@@ -5,6 +5,10 @@ function SiteHeader() {
   return (
     <div className={styles.wrapper}>
       <SiteNav />
+      <h1 className={styles.mainHeading}>Welcome to AI Cooking Assistant</h1>
+      <h3 className={styles.subheading}>
+        With us, you will never struggle to find meal ideas again.
+      </h3>
     </div>
   );
 }
