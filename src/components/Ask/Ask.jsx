668093bd904d3d06c920ed131b855046d4de3065
ChloeSAPage <chloesapagepersonal@gmail.com>
2024-05-21 10:56:49 +0100

    Add rendering of the info retrieved from the api

--- src/components/Ask/Ask.jsx
+++ src/components/Ask/Ask.jsx
@@ -4,12 +4,23 @@ import styles from "./Ask.module.css";
 import ButtonPurple from "../Reusable/ButtonPurple/ButtonPurple";
 import { makeAskRequest } from "./AskSlice";
 import { useDispatch, useSelector } from "react-redux";
+import Info from "../Info/Info";
 
 function Ask() {
   const result = useSelector((state) => state.ask.result);
   const dispatch = useDispatch();
   const [input, setInput] = useState("");
 
+  // Retrieve the info from result
+  const info = result.map((item) => {
+    return (
+      <Info
+      key={item.title}
+      title={item.title}
+      description={item.description}
+      />
+    )
+  })
 
   return (
     <div className={styles.wrapper}>
@@ -17,6 +28,7 @@ function Ask() {
       <ButtonPurple text={"Ask the brains"} onClick={() => dispatch(makeAskRequest())}/>
       <p>{input}</p>
       <p>{`Title: ${result[0].title} Description: ${result[0].description}`}</p>
+      <div>{info}</div>
     </div>
   );
 }
