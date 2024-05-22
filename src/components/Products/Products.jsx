03209f2cc6f1c998e324ca27d9ee1926c6fa0c04
Kamila W <kamila.wilczynska92@gmail.com>
2024-05-22 16:14:09 +0100

    add star rating

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,30 +1,8 @@
-// import styles from "./Products.module.css";
-// import fetchProducts from "./ProductSlice";
-// import { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-
-// function Products() {
-//   const productImage = useSelector((state) => state.product.imageURL);
-
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch(fetchProducts());
-//   }, [dispatch]);
-
-//   return (
-//     <div className={styles.wrapper}>
-//       Products
-//       {productImage && <img src={productImage} alt="product" />}
-//     </div>
-//   );
-// }
-
-// export default Products;
-
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "./ProductSlice";
+import styles from "./Products.module.css";
+import StarRating from "../Reusable/Rating/StarRating";
 
 function Products() {
   const products = useSelector((state) => state.products.items);
@@ -38,16 +16,24 @@ function Products() {
   const baseUrl = "https://project-2.technative.dev.f90.co.uk/";
 
   return (
-    <div>
-      {products.map((product) => (
-        <div key={product.id}>
-          <h2>{product.title}</h2>
-          <p>{product.description}</p>
-          <img src={baseUrl + product.image} alt={product.title} />
-          <p>Price: ${product.price}</p>
-          <p>Stars: {product.stars}</p>
+    <div className={styles.wrapper}>
+      <div className={` ${styles.card} ${styles.shadow} ${styles.padding}`}>
+        <div className={styles.productCards}>
+          {products.map((product) => (
+            <div className={styles.singleCard} key={product.id}>
+              <h2>{product.title}</h2>
+              <p>{product.description}</p>
+              <img
+                className={styles.productImage}
+                src={baseUrl + product.image}
+                alt={product.title}
+              />
+              <p>Price: ${product.price}</p>
+              <StarRating />
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 }
