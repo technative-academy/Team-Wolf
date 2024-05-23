c8e51a4d55d3397a7c77b71d71d75d62ace08156
ChloeSAPage <chloesapagepersonal@gmail.com>
2024-05-23 11:50:39 +0100

    Fix layout

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,27 +6,33 @@ function Home() {
   return (
     <div className={styles.wrapper}>
       <div className={` ${styles.card} ${styles.shadow} ${styles.padding}`}>
-        <h2 className={styles.margin}>We do Recipes</h2>
-        <h2 className={`${styles.margin} ${styles.via}`}>(via ChatGPT)</h2>
-        <p className={styles.para}>
-          Do you want a list of dishes that actually have no recipe at all? Ask
-          us a for them, we'll secretly pass it on to ChatGPT and will return
-          the result pretending that we generated it ourselves. Even this
-          paragraph was stolen.
-        </p>
-        <img src="/images/recipe-book.jpg" alt="image" />
-        <div className={styles.center}>
-          <p className={styles.para}>
-            Post a question about recipes and see some suggestions from our
-            specially trained Al
-          </p>
+        <div className={styles.titleContainer}>
+          <div>
+            <h2 className={styles.margin}>We do Recipes</h2>
+            <h2 className={`${styles.margin} ${styles.via}`}>(via ChatGPT)</h2>
+            <p className={styles.para}>
+              Do you want a list of dishes that actually have no recipe at all?
+              Ask us a for them, we'll secretly pass it on to ChatGPT and will
+              return the result pretending that we generated it ourselves. Even
+              this paragraph was stolen.
+            </p>
+          </div>
+          <img src="/images/recipe-book.jpg" alt="image" />
+        </div>
+
+        <div className={`${styles.buttonsContainer} ${styles.customBorder}`}>
           <div className={styles.buttonContainer}>
+            <p className={styles.para}>
+              Post a question about recipes and see some suggestions from our
+              specially trained Al
+            </p>
             <ButtonPurple text="Ask our AI!" />
           </div>
-          <p className={styles.para}>
-            Then don't actually make anything yourself, buy from us!
-          </p>
           <div className={styles.buttonContainer}>
+            <p className={styles.para}>
+              We make tasty treats! So don't actually make anything yourself,
+              buy from us! Please.
+            </p>
             <ButtonWhite text="Visit our Shop" />
           </div>
         </div>
