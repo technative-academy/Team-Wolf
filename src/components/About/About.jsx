e2533f61641c2b7b6f7599e30a5d6adb754a558c
Helder Balbino <helderbalbino@googlemail.com>
2024-05-24 13:11:25 +0100

    Improve about app page, removed h1 and imported font-awesome icons a list bullet points

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,31 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 import { useState } from "react";
 import styles from "./About.module.css";
-import { NavLink } from "react-router-dom";
 import AboutApp from "../AboutTheApp/AboutTheApp";
 
 function About() {
   const [activeLink, setActiveLink] = useState("about-app");
 
+  const handleNavLinkClick = (link) => {
+    setActiveLink(link);
+  };
+
   return (
     <div className={styles.wrapper}>
-      <div className={` ${styles.card} ${styles.shadow} ${styles.padding}`}>
+      <div className={`${styles.card} ${styles.shadow} ${styles.padding}`}>
         <div className={styles.buttonWrapper}>
           <NavLink
             to="about-app"
-            className={`${styles.link} ${
-              activeLink === "about-app" ? styles.activeLink : ""
-            }`}
-            onClick={() => setActiveLink("about-app")}
+            className={`${styles.link} ${activeLink === "about-app" ? styles.activeLink : ""}`}
+            onClick={() => handleNavLinkClick("about-app")}
           >
             About the App
           </NavLink>
           <NavLink
             to="about-team"
-            className={`${styles.link} ${
-              activeLink === "about-team" ? styles.activeLink : ""
-            }`}
-            onClick={() => setActiveLink("about-team")}
+            className={`${styles.link} ${activeLink === "about-team" ? styles.activeLink : ""}`}
+            onClick={() => handleNavLinkClick("about-team")}
           >
             About the Team
           </NavLink>
--- src/components/AboutTheApp/AboutTheApp.jsx
+++ src/components/AboutTheApp/AboutTheApp.jsx
@@ -1,44 +1,43 @@
 import styles from "./AboutTheApp.module.css";
 
-const AboutApp = () => {
+function AboutTheApp() {
   return (
-    <div>
-      <h2>About the App</h2>
-      <p>
-        Welcome to the AI Cooking Assistant, your ultimate companion in the
-        kitchen! Our intelligent assistant is designed to make meal planning and
-        cooking a breeze, whether you are a seasoned chef or just starting out.
-      </p>
-      <p>
-        <strone>Why Choose AI Cooking Assistant?</strone>
-      </p>
-      <ul>
-        <li>
-          Endless Meal Ideas: With our vast database of recipes and meal
-          suggestions, you’ll never struggle to come up with new and exciting
-          dishes. From quick weekday dinners to elaborate weekend feasts, we
-          have got you covered.
-        </li>
-        <li>
-          Personalized Recommendations: Our AI learns from your preferences and
-          dietary needs to provide customized meal suggestions. Whether you are
-          vegetarian, gluten-free, or have other specific requirements, we
-          tailor our recommendations to suit your lifestyle.
-        </li>
-        <li>
-          Easy to Use: Simply tell us what ingredients you have on hand, and our
-          assistant will suggest recipes that you can create with what you
-          already have. No more last-minute grocery runs!
-        </li>
-        <li>
-          Healthy and Nutritious Options: We prioritize your health by offering
-          a wide range of nutritious meal options. Our recipes are crafted to
-          provide balanced meals that nourish your body and delight your taste
-          buds.
-        </li>
-      </ul>
+    <div className={styles.wrapper}>
+      <div>
+        <p>
+          Welcome to the AI Cooking Assistant, your ultimate companion in the
+          kitchen! Our intelligent assistant is designed to make meal planning and
+          cooking a breeze, whether you are a seasoned chef or just starting out.
+        </p>
+        <p>Why Choose AI Cooking Assistant?</p>
+        <ul className={styles.iconList}>
+          <li>
+            Endless Meal Ideas: With our vast database of recipes and meal
+            suggestions, you’ll never struggle to come up with new and exciting
+            dishes. From quick weekday dinners to elaborate weekend feasts, we
+            have got you covered.
+          </li>
+          <li>
+            Personalized Recommendations: Our AI learns from your preferences and
+            dietary needs to provide customized meal suggestions. Whether you are
+            vegetarian, gluten-free, or have other specific requirements, we
+            tailor our recommendations to suit your lifestyle.
+          </li>
+          <li>
+            Easy to Use: Simply tell us what ingredients you have on hand, and our
+            assistant will suggest recipes that you can create with what you
+            already have. No more last-minute grocery runs!
+          </li>
+          <li>
+            Healthy and Nutritious Options: We prioritize your health by offering
+            a wide range of nutritious meal options. Our recipes are crafted to
+            provide balanced meals that nourish your body and delight your taste
+            buds.
+          </li>
+        </ul>
+      </div>
     </div>
   );
-};
+}
 
-export default AboutApp;
+export default AboutTheApp;
