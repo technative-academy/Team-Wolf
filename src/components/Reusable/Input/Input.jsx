6327b002a7d7710e324c4efb153c92b3d18d8472
ChloeSAPage <chloesapagepersonal@gmail.com>
2024-05-24 10:26:29 +0100

    Add styling

--- src/components/Reusable/Input/Input.jsx
+++ src/components/Reusable/Input/Input.jsx
@@ -1,14 +1,18 @@
 import styles from "./Input.module.css";
 
-function AskInput({ input, setInput, placeholder }) {
+function AskInput({ setInput, placeholder, handleEnter }) {
   function handleChange(event) {
     setInput(event.target.value);
   }
 
   return (
-    <>
-      <textarea className={styles.input} required type="text" placeholder={placeholder} onChange={handleChange} />
-    </>
+    <textarea
+      className={styles.input}
+      required
+      type="text"
+      placeholder={placeholder}
+      onChange={handleChange}
+    />
   );
 }
 
