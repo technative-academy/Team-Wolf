9b4e66d0c38b7e5f58f07bdc263d37782d6eeffa
ChloeSAPage <chloesapagepersonal@gmail.com>
2024-05-24 15:06:02 +0100

    Fix spacing on footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,12 +2,22 @@ import styles from "./Footer.module.css";
 import { Link } from "react-router-dom";
 
 function Footer() {
-  return <div className={styles.wrapper}>
-    <p> <Link to='#'> Terms and Conditions </Link> <Link to='#'> Cookies </Link> </p>
-    <div className={styles.copyRight}>
-    <p ><span>&#169;</span> 2024</p>
+  return (
+    <div className={styles.wrapper}>
+      <p>
+        {" "}
+        <Link to="#"> Terms and Conditions </Link>{" "}
+      </p>
+      <p>
+        <Link to="#"> Cookies </Link>
+      </p>
+      <div className={styles.copyRight}>
+        <p>
+          <span>&#169;</span> 2024
+        </p>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default Footer;
